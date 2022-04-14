490340cd3390098a87b9e878d8c97fd727c5d6e9
Uday <uday.kumar@perennialsys.com>
2022-04-14 16:22:51 +0530

    product

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navbar />
     <router-view />
   </div>
 </template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Navbar from "@/framework/sub-modules/pages/Navbar.vue";
+@Component({
+  components: {
+    Navbar,
+  },
+})
+export default class App extends Vue {}
+</script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,7 +31,7 @@
 #nav {
   padding: 30px;
 }
-
+/* 
 #nav a {
   font-weight: bold;
   color: #2c3e50;
@@ -28,5 +39,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/framework/sub-modules/pages/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -11,13 +11,15 @@ const routes: Array<RouteConfig> = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/product/:id",
+    name: "product",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import("@/framework/sub-modules/components/ProductDetail.vue"),
+  },
+  {
+    path: "/cart",
+    name: "cart",
+    component: () => import("@/framework/sub-modules/pages/Cart.vue"),
   },
 ];
 
