b7b23f8e796e78aa1be2c7c1120b2603a60d9243
Uday <uday.kumar@perennialsys.com>
2022-04-21 10:03:30 +0530

    removing console

--- public/sw.js
+++ public/sw.js
@@ -20,5 +20,5 @@ self.addEventListener("install", (e) => {
 });
 
 self.addEventListener("fetch", (e) => {
-  console.log(`Intercepting fetch request for: ${e.request.url}`);
+  // console.log(`${e.request.url}`);
 });
