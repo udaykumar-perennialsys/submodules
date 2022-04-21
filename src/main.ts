3bb563eee520dcced7807a1d1822b5232cb9b5c0
Uday <uday.kumar@perennialsys.com>
2022-04-21 09:22:20 +0530

    fix the github action

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./framework/sub-modules/store";
+import store from "@/framework/sub-modules/store";
 import "../public/addServiceWorker";
 
 Vue.config.productionTip = false;
